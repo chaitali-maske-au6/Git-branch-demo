59bc722f6887964bcf51bc1910494f297413974a
chaitali barde <maskechaitali56@gmail.com>
2020-03-02 15:09:33 +0530

    add feature-user and user route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express()
 
 app.get("/",function(req, res){
     res.send("hello world")
+});
+
+app.get("/users",function(req, res){
+    res.send("user route")
 })
 
 app.listen(1234,function(){
